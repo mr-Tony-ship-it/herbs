2223adc66598f46f87ccf6746a7a93e9dc383a23
Tony John <tonyjohn1903@outlook.com>
2025-02-23 14:33:23 +0530

    Update sendOtp.php

--- includes/sendOtp.php
+++ includes/sendOtp.php
@@ -4,7 +4,7 @@ require '../vendor/autoload.php'; // Ensure this points to the correct path
 use Mailgun\Mailgun;
 
 function sendOtp($recipient, $otp) {
-    $mgClient = Mailgun::create('64187d41d8b9f02ba735c157bdf03157-f6fe91d3-7b8871ad'); // Replace with your Mailgun API Key
+    $mgClient = Mailgun::create(''); // Replace with your Mailgun API Key
     $domain = "mail.herb.studio"; // Replace with your Mailgun domain
 
     $result = $mgClient->messages()->send($domain, [
