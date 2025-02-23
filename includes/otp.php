d8eb573eae8e1e500220e26ab853a0062fb2db85
Tony John <tonyjohn1903@outlook.com>
2025-02-23 14:32:03 +0530

    Update otp.php

--- includes/otp.php
+++ includes/otp.php
@@ -4,6 +4,6 @@ function sendOtp($to, $otp) {
     $message = "Your OTP code is: $otp";
     // Use mail() function to send email
     mail($to, $subject, $message);
-  //  45893f6f14530ffa3773e302d6a9c50b-72e4a3d5-aa1491f9
+  
 }
 ?>
